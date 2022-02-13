386e3573c616a4bf7f7e065c557dd095563c79ec
Beyza Ozdemir <beyzazdmr05@gmail.com>
2022-02-13 23:12:32 +0100

    General Design

--- Aim at Boxes/Assets/Ball.cs
+++ Aim at Boxes/Assets/Ball.cs
@@ -6,37 +6,48 @@ public class Ball : MonoBehaviour
 {
     float force;
     GameObject gameManager;
+    GameObject player;
 
     void Start()
     {
         force=20;
         gameManager=GameObject.FindWithTag("GameManager");
+        player=GameObject.FindWithTag("Player1Bomb");
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("OtherBoxes")){
                 collision.gameObject.GetComponent<other_boxes>().darbeal(force);
                 gameManager.GetComponent<GameManager>().createSoundandEfect(1,collision.gameObject);
-                
+                player.GetComponent<Player>().PowerBarMoving();
                 Destroy (gameObject);
-                //GetComponent<CircleCollider2D>().isTrigger = false;
+                
         }
-        if (collision.gameObject.CompareTag("Player2")){
+        if (collision.gameObject.CompareTag("Player2Bomb") || collision.gameObject.CompareTag("Player2Bomb")){
                 
                 gameManager.GetComponent<GameManager>().createSoundandEfect(1,collision.gameObject);
                 gameManager.GetComponent<GameManager>().hitBoxes(2,force);
+                player.GetComponent<Player>().PowerBarMoving();
                 Destroy (gameObject);
-                //GetComponent<CircleCollider2D>().isTrigger = false;
+                
         }
-        if (collision.gameObject.CompareTag("Player1")){
+        if (collision.gameObject.CompareTag("Player1Bomb") || collision.gameObject.CompareTag("Player1Bomb")){
                 
                 gameManager.GetComponent<GameManager>().createSoundandEfect(1,collision.gameObject);
                 gameManager.GetComponent<GameManager>().hitBoxes(1,force);
+                player.GetComponent<Player>().PowerBarMoving();
                 Destroy (gameObject);
-                //GetComponent<CircleCollider2D>().isTrigger = false;
+                
         }
+        if (collision.gameObject.CompareTag("Ground")){
+                
+                gameManager.GetComponent<GameManager>().createSoundandEfect(1,collision.gameObject);
+                player.GetComponent<Player>().PowerBarMoving();
+                Destroy (gameObject);
+                
+        }
+        
     }
-
     // Update is called once per frame
     void Update()
     {
--- Aim at Boxes/Assets/Player.cs
+++ Aim at Boxes/Assets/Player.cs
@@ -1,6 +1,8 @@
+using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -8,9 +10,41 @@ public class Player : MonoBehaviour
     public GameObject BallStartPoint;
     public ParticleSystem BallThrowingEfect;
     public AudioSource BallThrowingSound;
+
+    public Image PowerBar;
+    float powernum;
+    bool theend=false;//It means the bar didn't come to the end yet.
+    
+    Coroutine powerLoop;
+
     void Start()
     {
-        
+       powerLoop = StartCoroutine(PowerBarStart());
+    }
+
+    IEnumerator PowerBarStart(){
+
+        PowerBar.fillAmount=0;
+        theend=false;
+
+        while(true){
+            if(PowerBar.fillAmount<1 && !theend){
+                powernum = 0.01f;
+                PowerBar.fillAmount += powernum;
+                yield return new WaitForSeconds(0.0002f * Time.deltaTime);
+            }else
+            {
+                theend=true;
+                powernum = 0.01f;
+                PowerBar.fillAmount -= powernum;
+                yield return new WaitForSeconds(0.0002f * Time.deltaTime);
+                   
+                    if(PowerBar.fillAmount == 0){
+                        theend = false;
+                    }
+            
+            }
+        }
     }
 
     // Update is called once per frame
@@ -22,10 +56,15 @@ public class Player : MonoBehaviour
             BallThrowingSound.Play();
             GameObject ballObj = Instantiate(ball, BallStartPoint.transform.position, BallStartPoint.transform.rotation);
             Rigidbody2D rg = ballObj.GetComponent<Rigidbody2D>();
-            rg.AddForce(new Vector2(2f, 0f) * 10f, ForceMode2D.Impulse);
-            
-            
+            rg.AddForce(new Vector2(2f, 0f) * PowerBar.fillAmount * 15f, ForceMode2D.Impulse);
+       
+            StopCoroutine(powerLoop);
         }
-        
+
+    }
+
+    public void PowerBarMoving()
+    {
+       powerLoop = StartCoroutine(PowerBarStart());
     }
 }
