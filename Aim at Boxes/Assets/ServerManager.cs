5eae73949f0e3796a31dd75c5b7522d720a450f3
Beyza Ozdemir <beyzazdmr05@gmail.com>
2022-02-14 07:15:09 +0100

    Connecting Server

--- Aim at Boxes/Assets/ServerManager.cs
+++ Aim at Boxes/Assets/ServerManager.cs
@@ -1,13 +1,27 @@
+using System.Diagnostics;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Debug = UnityEngine.Debug;
 
-public class ServerManager : MonoBehaviour
+public class ServerManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster(){
+         Debug.Log("server hocam");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby(){
+        Debug.Log("Lobideyiz hocam");
     }
 
     // Update is called once per frame
